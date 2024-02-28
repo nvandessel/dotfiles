48f71c007275c820fab505f05e276816ad195df6
Nic van Dessel <51134175+nvandessel@users.noreply.github.com>
2024-02-27 16:48:41 -0800

    fix(nvim): disable wrap & swapfile + minor tweaks

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,9 +1,17 @@
 vim.opt.colorcolumn = "80"
 
--- Override tab length
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
