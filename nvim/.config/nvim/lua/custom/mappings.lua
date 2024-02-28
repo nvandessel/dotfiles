cf705b033baefb3fbb7ed2f59c7d0aea0594bfce
Nic van Dessel <51134175+nvandessel@users.noreply.github.com>
2024-02-27 18:14:29 -0800

    fix(nvim): apply remaps

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -1,11 +1,26 @@
 local M = {}
 
+M.disabled = {
+    ["<S-K>"] = "",
+}
+
 M.general = {
     n = {
         ["<C-h>"] = { "<cmd> TmuxNavigatorLeft<CR>", "window left" },
         ["<C-l>"] = { "<cmd> TmuxNavigatorRight<CR>", "window right" },
         ["<C-j>"] = { "<cmd> TmuxNavigatorDown<CR>", "window down" },
         ["<C-k>"] = { "<cmd> TmuxNavigatorUp<CR>", "window up" },
+        ["<C-d>"] = { "<C-d>zz", opts = {} },
+        ["<C-u>"] = { "<C-u>zz", opts = {} },
+        ["n"] = { "nzzzv", opts = {} },
+        ["N"] = { "Nzzzv", opts = {} },
+    },
+    v = {
+        ["J"] = { ":m '>+1<CR>gv=gv", opts = { noremap = true, silent = true } },
+        ["K"] = { ":m '<-2<CR>gv=gv", opts = { noremap = true, silent = true } },
+    },
+    x = {
+        ["<leader>p"] = { [["_dP]], opts = {} },
     }
 }
 
