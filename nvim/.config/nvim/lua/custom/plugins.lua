1692c9e8cc7426261bab598f76fdc654a9f1a612
Nic van Dessel <51134175+nvandessel@users.noreply.github.com>
2024-03-04 08:19:09 -0800

    feat(nvim): gh copilot

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -11,6 +11,16 @@ local plugins = {
         }
     },
     {
+        "zbirenbaum/copilot.lua",
+        lazy = false,
+        opts = function ()
+            return require "custom.configs.copilot"
+        end,
+        config = function (_, opts)
+            require("copilot").setup(opts)
+        end
+    },
+    {
         "williamboman/mason.nvim",
     },
     {
