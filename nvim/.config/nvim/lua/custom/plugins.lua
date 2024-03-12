1c18c051cb5356bfc1ac2cf1e33aacbb12a6dea1
Nic van Dessel <51134175+nvandessel@users.noreply.github.com>
2024-03-11 22:43:02 -0700

    feat(nvim): add ollama support

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -54,6 +54,40 @@ local plugins = {
             vim.g.rustfmt_autosave = 1
         end
     },
+    {
+      "nomnivore/ollama.nvim",
+      branch = "chat",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+
+      -- All the user commands added by the plugin
+      cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop", "OllamaChat", "OllamaChatClose" },
+
+
+      keys = {
+        -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+        {
+          "<leader>oo",
+          ":<c-u>lua require('ollama').prompt()<cr>",
+          desc = "ollama prompt",
+          mode = { "n", "v" },
+        },
+
+        -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+        {
+          "<leader>oG",
+          ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+          desc = "ollama Generate Code",
+          mode = { "n", "v" },
+        },
+      },
+
+      ---@type Ollama.Config
+      opts = {
+        -- your configuration overrides
+      }
+    },
 }
 
 return plugins;
