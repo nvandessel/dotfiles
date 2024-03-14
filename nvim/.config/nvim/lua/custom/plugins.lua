83d615a84d1f6afbd5ed2a89dcec3473ff6baf1f
Nic van Dessel <51134175+nvandessel@users.noreply.github.com>
2024-03-14 08:37:44 -0700

    feat(nvim): lazygit plugin

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -46,6 +46,20 @@ local plugins = {
         event = "VeryLazy",
         cmd = {"Git"},
     },
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
 
     -- File management
     {
